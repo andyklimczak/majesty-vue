68e3edafdd0c878a923cca6e12b1973ff4d0055d
Andy Klimczak <andyklimczak@fastmail.com>
2017-02-20 22:21:37 -0500

    15

---
+++ babel-example/assets/webpacked/app.js
@@ -0,0 +1,71 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var _client = __webpack_require__(1);
+
+	var pizza = 10;
+	var beer = 5;
+
+	var sum = function sum(a, b) {
+	  return a + b + '$';
+	};
+	console.log(_client.name + ': You have to pay ' + sum(pizza, beer));
+
+/***/ },
+/* 1 */
+/***/ function(module, exports) {
+
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	var name = exports.name = 'Andy';
+
+/***/ }
+/******/ ]);
---
+++ babel-example/gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const webpack = require('webpack-stream');
+
+gulp.task('default', ['babel']);
+
+gulp.task('babel', function() {
+  return gulp.src('src/*.js')
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(gulp.dest('assets/js'))
+});
+
+gulp.task('webpack', ['babel'], function() {
+  return gulp.src('assets/js/sum.js')
+    .pipe(webpack({
+      output: {
+        path: '/assets/webpacked',
+        filename: 'app.js'
+      }
+    }))
+    .pipe(gulp.dest('assets/webpacked'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/*.js', ['babel', 'webpack']);
+});
